0d6a123caaa50676b14e95c9f78d50cab628aecb
sundae95 <114928363+sundae95@users.noreply.github.com>
2023-05-08 17:24:22 +0900

    setup : 기본 파일 생성

---
+++ server/src/main/java/com/main12/hellomain/MainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.main12.hellomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainProjectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/main12/hellomain/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main12.hellomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
