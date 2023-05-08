f30ec7418047cb590e7f8554a32c8d31837bbb15
sundae95 <114928363+sundae95@users.noreply.github.com>
2023-05-08 18:34:30 +0900

    setup : 기본파일 생성

---
+++ server/src/test/java/com/main12/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main12.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
