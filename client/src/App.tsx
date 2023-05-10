910819dfe17e70557aaeedc7c64b17ceb59f9ef4
sung <sunga.jlh@gmail.com>
2023-05-10 19:55:12 +0900

    add : 히스토리, 고정된 북마크 조건부 렌더링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import { useState, SetStateAction, Dispatch } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { LightTheme } from './styles/theme/LightTheme';
 import TopNav from './components/TopNav';
+import History from './components/History';
+import CollectionPins from './components/CollectionPins';
 import Collections from './pages/Collections';
 import MyPage from './pages/MyPage';
 import Signup from './pages/Signup';
 import Main from './pages/Main';
 
-import './App.css';
-
 function App() {
+  const [showHistory, setShowHistory] = useState<boolean>(false);
+  const [showPinnedItems, setShowPinnedItems] = useState<boolean>(false);
+
   return (
     <div className="App">
       <ThemeProvider theme={LightTheme}>
         <GlobalStyle />
         <Router>
-          <TopNav />
+          <TopNav
+            showHistory={showHistory}
+            setShowHistory={setShowHistory}
+            showPinnedItems={showPinnedItems}
+            setShowPinnedItems={setShowPinnedItems}
+          />
+          {showHistory ? <History /> : null}
+          {showPinnedItems ? <CollectionPins /> : null}
           <Routes>
             <Route path="/" element={<Main />}>
               Main
--- client/src/components/TopNav.tsx
+++ client/src/components/TopNav.tsx
@@ -3,20 +3,37 @@ import { Link } from 'react-router-dom';
 import * as TN from '../styles/TopNavStyle';
 //상위 내비게이션 컴포넌트
 
-const TopNav = () => {
+type TopNavProps = {
+  showHistory: boolean;
+  setShowHistory: React.Dispatch<React.SetStateAction<boolean>>;
+  showPinnedItems: boolean;
+  setShowPinnedItems: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const TopNav = ({ showHistory, setShowHistory, showPinnedItems, setShowPinnedItems }: TopNavProps) => {
+  const handleHistoryBtnClick = () => {
+    setShowHistory(!showHistory);
+    if (showPinnedItems) setShowPinnedItems(false);
+  };
+
+  const handleCollectionsBtnClick = () => {
+    setShowPinnedItems(!showPinnedItems);
+    if (showHistory) setShowHistory(false);
+  };
+
   return (
     <TN.TopNavBox>
       <TN.LogoBox>
         <Link to="/">Chatcrawl</Link>
       </TN.LogoBox>
       <TN.NavIconsBox>
-        <span className="navitem">
+        <span className="navitem" onClick={handleHistoryBtnClick}>
           <Link to="/">History</Link>
         </span>
         <span className="navitem">
           <Link to="/">New Chat</Link>
         </span>
-        <span className="navitem">
+        <span className="navitem" onClick={handleCollectionsBtnClick}>
           <Link to="/bookmarks">Collections</Link>
         </span>
       </TN.NavIconsBox>
