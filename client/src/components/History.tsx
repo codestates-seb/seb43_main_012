31576f71d32ea69c5c56c7ceb0513a078cd3f426
zi0ne <jiwon0331j@gmail.com>
2023-05-12 02:23:52 +0900

    Feat: "히스토리 UI 헤더, 바디"
    - 헤더 바디 데이터박스 넣기 전

--- client/src/components/History.tsx
+++ client/src/components/History.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { ReactElement } from 'react';
+import {
+  SearchBox,
+  HistoryBox,
+  HistoryHeader,
+  Filtering,
+  DeleteButton,
+  HistoryBody,
+  TimeLine,
+} from '../styles/HistoryStyle';
 
-const History = () => {
-  return <div>History</div>;
-};
+function History(): ReactElement {
+  return (
+    <HistoryBox>
+      <HistoryHeader>
+        <SearchBox placeholder=" Search your history! tags (#node.js), title, content, date (3-15-2023, 3-2023)"></SearchBox>
+        <Filtering>Neweast</Filtering>
+        <DeleteButton>Clear History</DeleteButton>
+      </HistoryHeader>
+      <HistoryBody>
+        <TimeLine>TODAY</TimeLine>
+        <TimeLine>PREVIOUS 7DAYS</TimeLine>
+      </HistoryBody>
+    </HistoryBox>
+  );
+}
 
 export default History;
