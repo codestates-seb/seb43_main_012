a0682ab087061d4c5839c3cca47b7dbe2681fc9e
sung <sunga.jlh@gmail.com>
2023-05-11 16:10:59 +0900

    Feat: 유저아이콘 dialogbox 클릭한 위치에 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { useState, SetStateAction, Dispatch } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+//style
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { LightTheme } from './styles/theme/LightTheme';
+//pages & components
 import TopNav from './components/TopNav';
 import History from './components/History';
 import CollectionPins from './components/CollectionPins';
+import DialogBoxUserIcon from './components/dialogbox/DialogBoxUserIcon';
 import Collections from './pages/Collections';
 import MyPage from './pages/MyPage';
 import Signup from './pages/Signup';
 import Main from './pages/Main';
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false;
+// import { config } from '@fortawesome/fontawesome-svg-core';
+// import '@fortawesome/fontawesome-svg-core/styles.css';
+// config.autoAddCss = false;
 
 function App() {
   const [showHistory, setShowHistory] = useState<boolean>(false);
   const [showPinnedItems, setShowPinnedItems] = useState<boolean>(false);
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  const [isUserDialogOpen, setIsUserDialogOpen] = useState<boolean>(false);
+  const [dialogPosition, setDialogPosition] = useState({ x: 0, y: 0 });
 
   return (
     <div className="App">
@@ -31,10 +36,13 @@ function App() {
             showPinnedItems={showPinnedItems}
             setShowPinnedItems={setShowPinnedItems}
             isLoggedIn={isLoggedIn}
-            setIsLoggedIn={setIsLoggedIn}
+            isUserDialogOpen={isUserDialogOpen}
+            setIsUserDialogOpen={setIsUserDialogOpen}
+            setDialogPosition={setDialogPosition}
           />
-          {showHistory ? <History /> : null}
-          {showPinnedItems ? <CollectionPins /> : null}
+          {isUserDialogOpen && <DialogBoxUserIcon dialogPosition={dialogPosition} />}
+          {showHistory && <History />}
+          {showPinnedItems && <CollectionPins />}
           <Routes>
             <Route path="/" element={<Main />}>
               Main
--- client/src/components/dialogbox/DialogBoxUserIcon.tsx
+++ client/src/components/dialogbox/DialogBoxUserIcon.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DialogBox, UserInfo } from '../../styles/TopNavStyle';
+import styled from 'styled-components';
 
-const DialogBoxUserIcon = () => {
-  return <div>DialogBoxUserIcon</div>;
+type BoxProps = {
+  dialogPosition: { x: number; y: number };
+};
+
+type StyleProps = {
+  posX: number;
+  posY: number;
+};
+
+const MovingDialogBox = styled(DialogBox)<StyleProps>`
+  z-index: 1000;
+  left: ${p => p.posX - 200}px;
+  top: ${p => p.posY}px;
+  width: 200px;
+  height: 100px;
+  background: white;
+`;
+
+const DialogBoxUserIcon = ({ dialogPosition }: BoxProps) => {
+  console.log(`x: ${dialogPosition.x} `);
+  return (
+    <MovingDialogBox posX={dialogPosition.x} posY={dialogPosition.y}>
+      <UserInfo>DialogBoxUserIcon</UserInfo>
+    </MovingDialogBox>
+  );
 };
 
 export default DialogBoxUserIcon;
