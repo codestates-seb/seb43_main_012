8239fa467688dd9904b0a6451d36efd3134ae060
zi0ne <jiwon0331j@gmail.com>
2023-05-15 13:14:26 +0900

    Feat: "로그인 기능 구현"
    -로그인 유효성검사 후 정보 보내기"

--- client/src/api/loginApi.ts
+++ client/src/api/loginApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { request, requestAuth } from "./request";
 
 interface LoginArgs {
   userId: string;
@@ -6,22 +7,21 @@ interface LoginArgs {
   setErrors: any;
 }
 
-const LOGIN_URL = `http://localhost:3000/login`;
 
 export const handleLogin = async ({
   userId,
   password,
   setErrors,
 }: LoginArgs) => {
-  try {
-    const res = await axios.post(LOGIN_URL, {
+    const res = await requestAuth.post(`/login`, {
       userId,
       password,
     });
-    localStorage.setItem("token", res.headers["access-token"]);
-    window.location.replace("/");
-  } catch (error) {
-    setErrors(error);
-  }
-};
+    if(res.status !== 200) throw new Error(res.data.message)
+    sessionStorage.setItem("token", res.headers["Authorization"]);
+    sessionStorage.setItem("refresh", res.headers["refresh"]);
+    sessionStorage.setItem("user", JSON.stringify(res.data));
+    return res;
+  }; 
+
 
