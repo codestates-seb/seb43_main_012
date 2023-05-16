9af89cbab58a656f7cd299c7f846d8a7b63665b5
zi0ne <jiwon0331j@gmail.com>
2023-05-16 16:40:45 +0900

    Feat: "마이페이지 정보 확인 기능 구현"
    - 유저 정보 get

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import ModalEdit from '../components/modals/ModalEdit';
 import { MyPageWrapper, MyData } from '../styles/MyPageStyle';
 import { MainCharacter } from '../styles/CharacterStyle';
+import { handleUserInfo, UserInfoItemTypes } from '../api/MemberApi';
 
-const MyPage: React.FC = () => {
+function MyPage() {
   const [isOpen, setIsOpen] = useState(false);
+  const [avatarLink, setAvatarLink] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [userId, setUserId] = useState<string>('');
 
-  const email = `newname@naver.com`;
-  const displayName = `New Name`;
+  const Id = localStorage.getItem('memberId');
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const userData: UserInfoItemTypes = await handleUserInfo(`user/${Id}`);
+        setAvatarLink(userData.avatarLink); // avatarLink에 값 설정
+        setUsername(userData.username); // username 값 설정
+        setUserId(userData.userId); // userId 값 설정
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [Id]);
+
+  const email = userId;
+  const displayName = username;
+  const profileimg = avatarLink;
+
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -16,26 +39,33 @@ const MyPage: React.FC = () => {
   };
 
   const buttonClick = () => {
-		alert('회원 정보를 삭제합니다.');
+    alert('회원 정보를 삭제합니다.');
     navigate('/');
-	};
+  };
 
   return (
     <MyPageWrapper>
-      <MainCharacter>{displayName[0]}</MainCharacter>
-      <div className='modalbutton'>
+      <MainCharacter>
+        {setAvatarLink === setUsername ? (
+          displayName[0]
+        ) : (
+          <img src={profileimg} alt="" />
+        )}
+      </MainCharacter>
+      <div className="modalbutton">
         <button onClick={handleClick}>프로필 편집</button>
         <ModalEdit isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <MyData>{email}</MyData>
       <MyData>{`DisplayName: ${displayName}`}</MyData>
-      <div className='downbutton'>
-      <Link to="/"><button>로그아웃</button></Link>
-      <button onClick={buttonClick}>회원탈퇴</button>
+      <div className="downbutton">
+        <Link to="/">
+          <button>로그아웃</button>
+        </Link>
+        <button onClick={buttonClick}>회원탈퇴</button>
       </div>
     </MyPageWrapper>
-    
   );
-};
+}
 
 export default MyPage;
