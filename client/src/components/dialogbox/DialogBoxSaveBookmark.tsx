5a8a5bacdf02dbd3228587f3330b392cca2d97e6
sung <sunga.jlh@gmail.com>
2023-05-19 14:41:24 +0900

    Refactor: 북마크 등록/삭제 기능 optimized

--- client/src/components/dialogbox/DialogBoxSaveBookmark.tsx
+++ client/src/components/dialogbox/DialogBoxSaveBookmark.tsx
@@ -23,38 +23,11 @@ type Props = {
   setIsModalOpen: (isOpen: boolean) => void;
 };
 
-// type BookmarkCheckType = BookmarkType & {
-//   checked: boolean;
-// };
-
 const DialogBoxSaveBookmark = ({ setIsModalOpen }: Props) => {
   const dispatch = useAppDispatch();
   const cId = useAppSelector(selectCId);
   let bookmarks = useAppSelector(selectConversation).bookmarks;
   let uncheckedBookmarks = useAppSelector(selectConversation).bookmarkList;
-
-  const [bNum, setBNum] = useState<number>(bookmarks.length);
-
-  useEffect(() => {
-    console.log('update bookmarks in dialog box');
-    console.log('checked:', bookmarks);
-    console.log('unchecked: ', uncheckedBookmarks);
-  }, [bNum]);
-
-  // useEffect(() => {
-  //   console.log('fetching updated bookmarks!');
-  //   //temp solution
-  //   if (bookmarks) {
-  //     const newBookmarks = bookmarks.map((b) => {
-  //       return {
-  //         ...b,
-  //         checked: true,
-  //       };
-  //     });
-
-  //     setBookmarkList(newBookmarks);
-  //   }
-  // }, [bookmarks]);
 
   //access stored data for current conversation, that was fetched from the server
   //add property checked
@@ -63,12 +36,7 @@ const DialogBoxSaveBookmark = ({ setIsModalOpen }: Props) => {
   //   if (setIsOpen) setIsOpen(false);
   // };
 
-  // useEffect(() => {
-  //   console.log('change in bookmarkList');
-  // }, [bookmarkList]);
-
   //handle bookmark check/uncheck
-
   const handleBookmarkCheck = async ({
     id,
     newCheckValue,
@@ -85,37 +53,16 @@ const DialogBoxSaveBookmark = ({ setIsModalOpen }: Props) => {
           await dispatch(
             addBookmarkAsync({ cId, bId: id, bName: newBookmarkName }),
           );
-          setBNum((prev) => prev + 1);
         }
       } else {
         await dispatch(deleteBookmarkAsync({ cId, bId: id }));
-        setBNum((prev) => prev - 1);
       }
     } catch (error) {
       // Handle the error if needed
       console.error('Error occurred:', error);
     }
   };
-  // const handleBookmarkCheck = ({
-  //   id,
-  //   newCheckValue,
-  // }: {
-  //   id: number;
-  //   newCheckValue: boolean;
-  // }) => {
-  //   //if checked
-  //   if (newCheckValue) {
-  //     dispatch(updateBookmark({ bId: id, type: 'ADD' }));
-  //     setBNum((prev) => prev + 1);
-  //   } else {
-  //     dispatch(updateBookmark({ bId: id, type: 'DELETE' }));
-  //     setBNum((prev) => prev - 1);
-  //   }
-  // };
 
-  // const updateBNum = () => {
-  //   console.log('updating bookmark numbers');
-  // };
   return (
     <BookmarkList
       setIsModalOpen={setIsModalOpen}
