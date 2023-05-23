99c7a4bf6348e0ce4624dce31e4511b45b00baf4
sung <sunga.jlh@gmail.com>
2023-05-24 05:51:57 +0900

    Feat: 대화 삭제 컨펌 UI

--- client/src/components/history/HistoryEditUI.tsx
+++ client/src/components/history/HistoryEditUI.tsx
@@ -4,12 +4,13 @@ import styled from 'styled-components';
 import { ReactComponent as DeleteIcon } from '../../assets/icons/history/iconDelete.svg';
 import { ReactComponent as PinnedIcon } from '../../assets/icons/history/iconPinned.svg';
 import { ReactComponent as PinIcon } from '../../assets/icons/history/iconPinOff.svg';
+import { ReactComponent as ConfirmIcon } from '../../assets/icons/history/iconCheckAlternative.svg';
+import { ReactComponent as CancelIcon } from '../../assets/icons/main_qna/iconCancel.svg';
 
 const EditUIBox = styled.div`
   color: var(--color-default-yellow);
   display: flex;
   flex-direction: row;
-  //   width: 100%;
   justify-content: center;
   align-items: center;
   align-self: flex-end;
@@ -20,9 +21,32 @@ const EditUIBox = styled.div`
     display: flex;
   }
   svg:hover {
-    // color: var(--color-default-yellow-darker);
-    color: var(--color-default-green);
-    // color: black;
+    color: var(--color-default-yellow-darker);
+    // color: var(--color-default-green);
+  }
+`;
+
+const ConfirmCancelBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: var(--color-error);
+  font-size: 14px;
+  font-weight: 600;
+
+  //   svg {
+  //     path {
+  //       stroke-width: 3;
+  //     }
+  //   }
+
+  svg:hover {
+    path {
+      color: var(--color-error);
+      stroke-width: 3;
+    }
+    // color: darkred !important;
   }
 `;
 
@@ -31,8 +55,14 @@ type Props = {
   handlePinUpdate: (newPinValue: boolean) => void;
 };
 const HistoryEditUI = ({ pinned, handlePinUpdate }: Props) => {
-  const [isPinned, setIsPinned] = useState(pinned);
+  const [aboutToDelete, setAboutToDelete] = useState<boolean>(false);
+  const [isPinned, setIsPinned] = useState<boolean>(pinned);
 
+  const handleDeleteClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setAboutToDelete(true);
+  };
   const handlePinClick = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
@@ -40,14 +70,38 @@ const HistoryEditUI = ({ pinned, handlePinUpdate }: Props) => {
     handlePinUpdate(!isPinned);
   };
 
+  const handleConfirmClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setAboutToDelete(false);
+  };
+
+  const handleCancelClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setAboutToDelete(false);
+  };
+
   return (
     <EditUIBox>
-      {isPinned ? (
-        <PinnedIcon onClick={handlePinClick} />
+      {aboutToDelete ? (
+        <ConfirmCancelBox>
+          <div>{`DELETE ?`}</div>
+          <>
+            <ConfirmIcon onClick={handleConfirmClick} />
+            <CancelIcon onClick={handleCancelClick} />
+          </>
+        </ConfirmCancelBox>
       ) : (
-        <PinIcon onClick={handlePinClick} />
+        <>
+          {isPinned ? (
+            <PinnedIcon onClick={handlePinClick} />
+          ) : (
+            <PinIcon onClick={handlePinClick} />
+          )}
+          <DeleteIcon onClick={handleDeleteClick} />
+        </>
       )}
-      <DeleteIcon />
     </EditUIBox>
   );
 };
