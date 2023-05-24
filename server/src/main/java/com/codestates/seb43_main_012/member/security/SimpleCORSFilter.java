4b683937214cbfb284a678e6166530ba97afe78a
splo-sh <105478842+splo-sh@users.noreply.github.com>
2023-05-24 10:29:48 +0900

    Fix: Cors설정

--- server/src/main/java/com/codestates/seb43_main_012/member/security/SimpleCORSFilter.java
+++ server/src/main/java/com/codestates/seb43_main_012/member/security/SimpleCORSFilter.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, http://chatcrawlbucket.s3-website.ap-northeast-2.amazonaws.com");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "ngrok-skip-browser-warning, Authorization, Cookie, Access-Control-Allow-Origin, Access-Control-Allow-Methods," +
