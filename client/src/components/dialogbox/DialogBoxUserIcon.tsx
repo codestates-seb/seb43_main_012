fe745223f12595aa380f1072658abf8f24203a02
sung <sunga.jlh@gmail.com>
2023-05-28 23:43:51 +0900

    Fix: 유저정보 api 부르기 에러 고침 + 테스트 주석처리

--- client/src/components/dialogbox/DialogBoxUserIcon.tsx
+++ client/src/components/dialogbox/DialogBoxUserIcon.tsx
@@ -54,7 +54,6 @@ const DialogBoxUserIcon = ({
   ) => {
     //close modal
     e.stopPropagation();
-    console.log('dialog item clicked!');
     if (setIsUserDialogOpen) setIsUserDialogOpen(false);
   };
 
